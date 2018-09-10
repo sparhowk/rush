b4b27b28b3a5f62d0a654a63c83f926b08ca0415
sparhowkDell <sparhowk@users.noreply.github.com>
2018-09-10 14:45:56 +0200

    level09.lesson02.task05

---
+++ src/com/javarush/test/level09/lesson02/task05/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.test.level09.lesson02.task05;
+
+/* The method should return a result - its stack trace depth
+Write a method that displays and returns its stack trace depth. Stack trace depth is the number of its methods (the number of elements in the list).
+*/
+
+public class Solution
+{
+    public static int getStackTraceDeep()
+    {
+        //add your code here
+
+    }
+}
