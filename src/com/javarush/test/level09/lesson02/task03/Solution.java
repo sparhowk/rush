34a42d7ff56e4ed601890bde474ecaa2124eb281
sparhowkDell <sparhowk@users.noreply.github.com>
2018-09-10 14:17:08 +0200

    level09.lesson02.task03

---
+++ src/com/javarush/test/level09/lesson02/task03/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.test.level09.lesson02.task03;
+
+/* The method should return the line number of the code this method was called from
+Write five methods that call each other. Each method should return the line number of the code this method was called from.
+    Use the function element.getLineNumber().
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        method1();
+    }
+
+    public static int method1()
+    {
+        method2();
+        return  /*add your code here*/ 0;
+    }
+
+    public static int method2()
+    {
+        method3();
+        return  /*add your code here*/ 0;
+    }
+
+    public static int method3()
+    {
+        method4();
+        return  /*add your code here*/ 0;
+    }
+
+    public static int method4()
+    {
+        method5();
+        return  /*add your code here*/ 0;
+    }
+
+    public static int method5()
+    {
+        return  /*add your code here*/ 0;
+    }
+}
