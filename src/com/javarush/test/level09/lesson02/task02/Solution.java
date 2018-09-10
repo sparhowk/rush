34edc7cce248eb47c7bd40da72835a0fbff3bd04
sparhowkDell <sparhowk@users.noreply.github.com>
2018-09-10 13:20:53 +0200

    level09.lesson02.task02

---
+++ src/com/javarush/test/level09/lesson02/task02/Solution.java
@@ -0,0 +1,48 @@
+package com.javarush.test.level09.lesson02.task02;
+
+/* StackTrace again
+Write five methods that call each other. Each method should return the name of its caller method.
+    You can obtain caller method using StackTrace.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        method1();
+    }
+
+    public static String method1()
+    {
+        method2();
+        //add your code here
+
+    }
+
+    public static String method2()
+    {
+        method3();
+        //add your code here
+
+    }
+
+    public static String method3()
+    {
+        method4();
+        //add your code here
+
+    }
+
+    public static String method4()
+    {
+        method5();
+        //add your code here
+
+    }
+
+    public static String method5()
+    {
+        //add your code here
+
+    }
+}
