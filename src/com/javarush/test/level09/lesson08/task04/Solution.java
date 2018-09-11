1053be6836630ae4cfd8ceed5c8dd274f9edb7af
sparhowk <sparhowk@wp.pl>
2018-09-11 15:55:16 +0200

    level09.lesson08.task04

--- src/com/javarush/test/level09/lesson08/task04/Solution.java
+++ src/com/javarush/test/level09/lesson08/task04/Solution.java
@@ -16,9 +16,23 @@ public class Solution {
     }
 
     public static void processExceptions(Solution obj) {
-        obj.method1();
-        obj.method2();
-        obj.method3();
+        try {
+            obj.method1();
+            obj.method2();
+            obj.method3();
+        }
+        catch (RemoteException e)
+        {
+            System.out.println(e);
+        }
+        catch (NoSuchFieldException e)
+        {
+            System.out.println(e);
+        }
+        catch (IOException e)
+        {
+            System.out.println(e);
+        }
     }
 
     public void method1() throws IOException {
