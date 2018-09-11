67973cc5f1b6230d45cd5d9dc0e775662b2a1b46
sparhowk <sparhowk@wp.pl>
2018-09-11 11:58:20 +0200

    level09.lesson06.task03

---
+++ src/com/javarush/test/level09/lesson06/task03/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.test.level09.lesson06.task03;
+
+/* Exception when operating with arrays
+Catch the exception that occurs when run the code:
+    int[] m = new int[2];
+    m[8] = 5;
+    Display the exception to the screen, specifying its type.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        //add your code here
+
+        int[] m = new int[2];
+        m[8]= 5;
+
+        //add your code here
+
+    }
+}
