7639f015557714bd347e79ac5d05ec912301e835
sparhowk <sparhowk@wp.pl>
2018-09-11 11:40:58 +0200

    level09.lesson06.task02

---
+++ src/com/javarush/test/level09/lesson06/task02/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.test.level09.lesson06.task02;
+
+/* Exception when operating with strings
+Catch the exception that occurs when run the code:
+    String s = null;
+    String m = s.toLowerCase();
+    Display the exception to the screen, specifying its type.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        //add your code here
+
+        String s = null;
+        String m = s.toLowerCase();
+
+        //add your code here
+
+    }
+}
