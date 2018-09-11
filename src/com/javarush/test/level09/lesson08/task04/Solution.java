fc73eb4a9072536ff1cb550f0722a812972272db
sparhowk <sparhowk@wp.pl>
2018-09-11 15:54:35 +0200

    level09.lesson08.task03

---
+++ src/com/javarush/test/level09/lesson08/task04/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.test.level09.lesson08.task04;
+
+import java.io.IOException;
+import java.rmi.RemoteException;
+
+/* Catching checked exceptions
+Handle all the checked exceptions in the method processExceptions().
+    You need to display to the screen each checked exception that has occurred.
+    You may use only one block try.
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        processExceptions(new Solution());
+
+    }
+
+    public static void processExceptions(Solution obj) {
+        obj.method1();
+        obj.method2();
+        obj.method3();
+    }
+
+    public void method1() throws IOException {
+        throw new IOException();
+    }
+
+    public void method2() throws NoSuchFieldException {
+        throw new NoSuchFieldException();
+    }
+
+    public void method3() throws RemoteException {
+        throw new RemoteException();
+    }
+}
