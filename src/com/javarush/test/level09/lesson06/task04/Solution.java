318d851842aab37337854f4ceb491003cf4b674a
sparhowk <sparhowk@wp.pl>
2018-09-11 11:59:39 +0200

    level09.lesson06.task04

---
+++ src/com/javarush/test/level09/lesson06/task04/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.test.level09.lesson06.task04;
+
+import java.util.ArrayList;
+
+/* Exception when operating with List collections
+Catch the exception that occurs when run the code:
+    ArrayList<String> list = new ArrayList<String>();
+    String s = list.get(18);
+    Display the exception to the screen, specifying its type.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        //add your code here
+
+        ArrayList<String> list = new ArrayList<String>();
+        String s = list.get(18);
+
+        //add your code here
+
+    }
+}
