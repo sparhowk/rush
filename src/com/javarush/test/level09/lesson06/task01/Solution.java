84509878cd199448548407eaefd46689355bc72a
sparhowk <sparhowk@wp.pl>
2018-09-11 11:09:09 +0200

    level09.lesson06.task01

---
+++ src/com/javarush/test/level09/lesson06/task01/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.test.level09.lesson06.task01;
+
+/* Exception when operating with numbers
+Catch the exception that occurs when run the code:
+    int a = 42 / 0;
+    Display the exception to the screen, specifying its type.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        //add your code here
+
+         int a = 42/0;
+
+        //add your code here
+
+    }
+}
