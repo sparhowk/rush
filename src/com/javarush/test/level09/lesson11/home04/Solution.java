b9063f2185e1a218969ed2cf3bdc7e09fd0a6e99
sparhowk <sparhowk@wp.pl>
2018-09-12 15:18:47 +0200

    level09.lesson11.home04

---
+++ src/com/javarush/test/level09/lesson11/home04/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.test.level09.lesson11.home04;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/* Date converter
+Read from the keyboard a date in the format «08/18/2013»
+    Display to the screen that date in the form of «AUG 18, 2013».
+    Use the objects Date and SimpleDateFormat.
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        //add your code here
+    }
+}
