9063ae847fa8c27c8f824a045c5bcfe18ffe474a
sparhowk <sparhowk@wp.pl>
2018-09-12 10:06:54 +0200

    level09.lesson11.home01

---
+++ src/com/javarush/test/level09/lesson11/home01/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.test.level09.lesson11.home01;
+
+/* Division by zero
+    Create a method public static void divisionByZero(), where you need to divide any number by zero,
+    and display to the screen the result of the division.
+    Wrap the divisionByZero() method call into a try..catch.
+    Display to the screen the exception stack trace using the method exception.printStackTrace().
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        divisionByZero();
+    }
+
+    private static void divisionByZero() {
+    }
+}
