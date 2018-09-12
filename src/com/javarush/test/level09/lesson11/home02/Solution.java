e13d4ea339ca8257a813da6c98f2ea744b2e0b1e
sparhowk <sparhowk@wp.pl>
2018-09-12 10:37:41 +0200

    level09.lesson11.home02

---
+++ src/com/javarush/test/level09/lesson11/home02/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.test.level09.lesson11.home02;
+
+/* Countdown from 10 to 0
+Write a loop to countdown from 10 to 0. Use Thread.sleep(100) to make a delay;
+    Wrap the sleep call into a try..catch.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        for (int i = 10; i >= 0; i--)
+        {
+            System.out.println(i);
+
+            //add your code here
+        }
+    }
+}
