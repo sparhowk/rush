96e08d73138b62b7cb54992e73f859528edd5a21
sparhowk <sparhowk@wp.pl>
2018-09-18 10:49:14 +0200

    level10.lesson11.home01

---
+++ src/com/javarush/test/level10/lesson11/home01/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.test.level10.lesson11.home01;
+
+/* The correct answer is: d=2.941
+Add one type casting operator to obtain d=2.941.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        int a = 50;
+        int b = 17;
+        double d = a / b;
+        System.out.println(d);
+    }
+}
