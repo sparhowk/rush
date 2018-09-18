d6ac3d2380e93477a459881344c9a3536a39f134
sparhowk <sparhowk@wp.pl>
2018-09-18 10:53:36 +0200

    level10.lesson11.home02

---
+++ src/com/javarush/test/level10/lesson11/home02/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.test.level10.lesson11.home02;
+
+/* The correct answer is: d=5.5
+Add one type casting operator to obtain d=5.5.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        int a = 5;
+        int b = 4;
+        int c = 3;
+        int e = 2;
+        double d = a + b/c/e;
+        System.out.println(d);
+    }
+}
