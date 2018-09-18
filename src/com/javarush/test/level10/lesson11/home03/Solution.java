9a0533457fa595f7df61c3d4218077d8fe1a753b
sparhowk <sparhowk@wp.pl>
2018-09-18 11:25:41 +0200

    level10.lesson11.home03

---
+++ src/com/javarush/test/level10/lesson11/home03/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.test.level10.lesson11.home03;
+
+/* The correct answer is: d=1.0
+Add one type casting operator to obtain d=1.0.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        int a = 257;
+        int b = 4;
+        int c = 3;
+        int e = 2;
+        double d = a + b/c/e;
+
+        System.out.println(d);
+    }
+}
