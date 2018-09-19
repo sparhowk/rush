5b58ecea3aaa6f80fbaf5833a23eb7361bd8d986
sparhowk <sparhowk@wp.pl>
2018-09-19 14:45:49 +0200

    level10.lesson11.bonus02 - ver 1.0

--- src/com/javarush/test/level10/lesson11/bonus02/Solution.java
+++ src/com/javarush/test/level10/lesson11/bonus02/Solution.java
@@ -1,10 +1,13 @@
 package com.javarush.test.level10.lesson11.bonus02;
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /* Add new functionality to the program.
-Old task: The program should display a pair (a number and a string) entered from the keyboard.
-    New task: The program should store in HashMap pairs (a number and a string) entered from the keyboard. An empty string means the end of input. Numbers may be repeated. Strings are always unique. Input data must not be lost!
+    Old task: The program should display a pair (a number and a string) entered from the keyboard.
+    New task: The program should store in HashMap pairs (a number and a string) entered from the keyboard.
+    An empty string means the end of input. Numbers may be repeated.     Strings are always unique. Input data must not be lost!
     The program should display the contents of HashMap to the screen.
 
     Example input:
@@ -23,10 +26,22 @@ public class Solution
     public static void main(String[] args) throws IOException
     {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        HashMap<Integer, String> idAndNames = new HashMap<Integer, String>();
 
-        int id = Integer.parseInt(reader.readLine());
-        String name = reader.readLine();
+        while (true)
+        {
+            String idString = reader.readLine();
+            if (idString.isEmpty()) break;
 
-        System.out.println("Id=" + id + " Name=" + name);
+            String name = reader.readLine();
+            if (name.equals("")) break;
+
+            int id = Integer.parseInt(idString);
+            idAndNames.put(id, name);
+        }
+
+        for (Map.Entry<Integer, String> map: idAndNames.entrySet()) {
+            System.out.println(map.getKey() + " " + map.getValue());
+        }
     }
 }
